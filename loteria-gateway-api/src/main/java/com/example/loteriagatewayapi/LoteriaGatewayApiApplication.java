5a7ea0072ea164a79178f42ca9a0fc44dc4fd463
Adriano Dias <adriano.rj.br@gmail.com>
2023-04-27 19:26:17 -0300

    implementacao gateway

---
+++ loteria-gateway-api/src/main/java/com/example/loteriagatewayapi/LoteriaGatewayApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.loteriagatewayapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoteriaGatewayApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoteriaGatewayApiApplication.class, args);
+    }
+
+}
---
+++ loteria-gateway-api/src/test/java/com/example/loteriagatewayapi/LoteriaGatewayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loteriagatewayapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoteriaGatewayApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
